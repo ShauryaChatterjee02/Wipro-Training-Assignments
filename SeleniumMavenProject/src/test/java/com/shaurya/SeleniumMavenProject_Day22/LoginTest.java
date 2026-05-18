364595158b03d79842c8803ba591c92578f614dc
Shaurya Chattopadhyay <117979507+ShauryaChatterjee02@users.noreply.github.com>
2026-05-18 18:05:25 +0530

    Add files via upload

---
+++ SeleniumMavenProject/src/test/java/com/shaurya/SeleniumMavenProject_Day22/LoginTest.java
@@ -0,0 +1,9 @@
+package com.shaurya.SeleniumMavenProject_Day22;
+
+public class LoginTest {
+    public static void main(String[] args) {
+
+        System.out.println("Program Running");
+
+    }
+}
